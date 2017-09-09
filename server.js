1a3cd466f58f06b35103bab119960b7ed6e83c15
Toma Nistor <toma.nistor@gmail.com>
2017-09-09 00:45:17 -0700

    Added static page and server access logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
 // Create server
 const server = new Hapi.Server();
@@ -26,10 +27,50 @@ server.route({
   }
 })
 
-// Start server
-server.start((err) => {
+
+// Static page
+server.register(require('inert'), (err) => {
   if (err) {
     throw err;
   }
-  console.log(`Server running at: ${server.info.uri}`);
+
+  server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+      reply.file('./public/hello.html');
+    }
+  });
 });
+
+// Start server with access logging
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}, (err) => {
+  if (err) {
+    throw err; // Something bad happened loading the plugin
+  }
+
+  // Start server
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+
+})
