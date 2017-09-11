9b6df11df1ee7cbce4ec61157ee8ee3459d3ad3e
Toma Nistor <toma.nistor@gmail.com>
2017-09-10 22:09:23 -0700

    Served public folder directory

--- server.js
+++ server.js
@@ -1,50 +1,65 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 const Good = require('good');
+const Inert = require('inert');
 
-// Create server
-const server = new Hapi.Server();
+
+// Create server with public/ as starting directory
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
+      }
+    }
+  }
+});
 server.connection({
   port: 3000,
   host: 'localhost'
 });
 
+
 // Routes
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    return reply('Hello World!');
-  }
-});
+// server.route({
+//   method: 'GET',
+//   path: '/',
+//   handler: function (request, reply) {
+//     return reply('Hello World!');
+//   }
+// });
 
 server.route({
   method: 'GET',
   path: '/{name}',
   handler: function (request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    reply('Hello, I can\'t find the page for ' + encodeURIComponent(request.params.name) + '!');
   }
-})
+});
 
 
 // Static page
-server.register(require('inert'), (err) => {
-  if (err) {
-    throw err;
-  }
+// server.register(require('inert'), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//
+//   server.route({
+//     method: 'GET',
+//     path: '/hello',
+//     handler: function (request, reply) {
+//       reply.file('./public/hello.html');
+//     }
+//   });
+// });
 
-  server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
-      reply.file('./public/hello.html');
-    }
-  });
-});
 
-// Start server with access logging
-server.register({
+// Start server with:
+// - Access logging (Good)
+// - Static pages (Inert)
+server.register([{
   register: Good,
   options: {
     reporters: {
@@ -60,10 +75,35 @@ server.register({
       }, 'stdout']
     }
   }
-}, (err) => {
+}, {
+  register: Inert
+}], (err) => {
   if (err) {
-    throw err; // Something bad happened loading the plugin
+    throw err; // Error loading plugins
   }
+
+  // Route in traditional form
+  server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+      reply.file('hello.html');
+    }
+  });
+
+  // Route using file handler
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+          index: true,
+          defaultExtension: 'html'
+        }
+    }
+  });
 
   // Start server
   server.start((err) => {
@@ -73,4 +113,4 @@ server.register({
     server.log('info', 'Server running at: ' + server.info.uri);
   });
 
-})
+});
